5fb6b51e3765ad9b911d352e52f8a24f3c766cad
enchooi <121946331+enchooi@users.noreply.github.com>
2024-08-12 17:15:39 +0900

    ajax / fetch

---
+++ springweb23ajaxdb/src/main/java/pack/Springweb23ajaxdbApplication.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springweb23ajaxdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springweb23ajaxdbApplication.class, args);
+	}
+
+}
