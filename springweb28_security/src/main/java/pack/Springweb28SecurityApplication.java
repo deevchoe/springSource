001beb052a35fd3bd7fb981df7d2d6e98090e296
enchooi <121946331+enchooi@users.noreply.github.com>
2024-08-27 12:01:37 +0900

    스프링 시큐리티

---
+++ springweb28_security/src/main/java/pack/Springweb28SecurityApplication.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springweb28SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springweb28SecurityApplication.class, args);
+	}
+
+}
