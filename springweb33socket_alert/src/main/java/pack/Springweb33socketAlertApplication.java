436653aadaed9ba008e42577541c4a7de890c99b
enchooi <121946331+enchooi@users.noreply.github.com>
2024-09-05 11:02:53 +0900

    웹 소켓 알림

---
+++ springweb33socket_alert/src/main/java/pack/Springweb33socketAlertApplication.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springweb33socketAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springweb33socketAlertApplication.class, args);
+	}
+
+}
